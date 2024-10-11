69e47998410341223ffeed7fc398e87ab6849048
Nehal garg <nehalgarg@vl965-172-31-135-62.wireless.umass.edu>
2024-10-11 13:11:33 -0400

    adding test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -17,3 +17,16 @@ describe('Calculator Calculation Functions', function() {
 
 
 // Add your tests here
+describe('Calculator Calculation Functions', function() {
+    it('should return 2 for the square root of 4', function() {
+        const res = calculator.calculateSquareRoot(4);
+        assert.strictEqual(res, 2);
+    });
+
+    it('should return 3 for the square root of 9', function() {
+        const res = calculator.calculateSquareRoot(9);
+        assert.strictEqual(res, 3);
+    });
+
+
+});
